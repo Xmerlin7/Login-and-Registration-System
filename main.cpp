6acde84ce5618a7a2164aefa22fe823a98df446c
Seif Allah <saifeldarageely@gmail.com>
2024-03-30 23:12:46 +0200

    #

--- main.cpp
+++ main.cpp
@@ -1,22 +1,21 @@
 #include "UserManager.h"
-using namespace std;
 int main()
 {
     UserManager usermanage;
-
+    std::cout << "\n\n\t\t1. Register User " << std::endl;
     int op;
     char choice;
     do
     {
-        system("cls");
-        cout << "\n\n\t\t1. Register User " << endl;
-        cout << "\t\t2.Login " << endl;
-        cout << "\t\t3. Show User List " << endl;
-        cout << "\t\t4. Search User " << endl;
-        cout << "\t\t5. Delete User" << endl;
-        cout << "\t\t6. Exit" << endl;
-        cout << "\t\tEnter Your Choice : ";
-        cin >> op;
+        // system("cls");
+        std::cout << "\n\n\t\t1. Register User " << std::endl;
+        std::cout << "\t\t2.Login " << std::endl;
+        std::cout << "\t\t3. Show User List " << std::endl;
+        std::cout << "\t\t4. Search User " << std::endl;
+        std::cout << "\t\t5. Delete User" << std::endl;
+        std::cout << "\t\t6. Exit" << std::endl;
+        std::cout << "\t\tEnter Your Choice : ";
+        std::cin >> op;
         switch (op)
         {
         case 1:
@@ -26,11 +25,11 @@ int main()
         }
         case 2:
         {
-            string username, password;
-            cout << "\t\tEnter User :";
-            cin >> username;
-            cout << "\t\tEnter Password :";
-            cin >> password;
+            std::string username, password;
+            std::cout << "\t\tEnter User :";
+            std::cin >> username;
+            std::cout << "\t\tEnter Password :";
+            std::cin >> password;
             usermanage.loginUser(username, password);
             break;
         }
@@ -41,24 +40,24 @@ int main()
         }
         case 4:
         {
-            string username;
-            cout << "\t\tEnter User Name :";
-            cin >> username;
+            std::string username;
+            std::cout << "\t\tEnter User Name :";
+            std::cin >> username;
             usermanage.searchUser(username);
             break;
         }
         case 5:
         {
-            string username;
-            cout << "\t\tEnter User Name :";
-            cin >> username;
+            std::string username;
+            std::cout << "\t\tEnter User Name :";
+            std::cin >> username;
             usermanage.deleteUser(username);
             break;
         }
         case 6:
             exit(1);
         }
-        cout << "\t\tDo You Want to Continue [Yes/ No] ? :";
-        cin >> choice;
+        std::cout << "\t\tDo You Want to Continue [Yes/ No] ? :";
+        std::cin >> choice;
     } while (choice == 'y' || choice == 'Y');
 }
