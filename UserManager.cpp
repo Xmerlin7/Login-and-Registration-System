fc38bbfe29f58bffe0644d4e6152990cc8248b6a
Seif Allah <saifeldarageely@gmail.com>
2024-03-30 22:43:22 +0200

    Adding register feature

--- UserManager.cpp
+++ UserManager.cpp
@@ -2,19 +2,33 @@
 
 void UserManager::RegisterUser()
 {
+    bool flag;
     std::string username, password;
     std::cout << "Please enter your username: ";
     std::cin >> username;
     std::cout << "Please enter your password: ";
     std::cin >> password;
+    for (int i = 0; i < users.size(); i++)
+    {
+        if ((users[i].getUsername() == username) && (users[i].getUserPass() == password))
+            flag = false
+    }
+    flag = true;
     /*check that is not alread registerd*/
-    // Create a new User object using the entered username and password
-    User newUser(username, password);
+    if (flag)
+    {
+        // Create a new User object using the entered username and password
+        User newUser(username, password);
 
-    // Add the new user to the vector of users
-    users.push_back(newUser);
+        // Add the new user to the vector of users
+        users.push_back(newUser);
 
-    std::cout << "User registered successfully.\n";
+        std::cout << "User registered successfully.\n";
+    }
+    else
+    {
+        std::cout << "User already registered try to log in.\n";
+    }
 }
 
 bool UserManager::LoginUser(std::string name, std::string pass)
